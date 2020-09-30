a4cb3ab4793be35942e78dea59ba24f748fe023f
Mark Jarvis <jarkmarvis@gmail.com>
2020-09-29 23:35:50 -0400

    CNC Specific Updates
    Adding Dual Y-Axis using Y Pins nad E0 Pins
    Setting Custom Bed Size
    Using Min and Max hardware stops
    Step/mm Calibration
    Inverting Y-Axis Direction
    Adding Z-Probe Zero Offset
    Adding Custom Code in Pins file to force Marlin to accept Max Endstop pins
    Some updates sourced from the MPCNC project Lots of great information on the SKR 1.4 Turbo there.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -498,7 +498,7 @@
 
 // If you want endstops to stay on (by default) even when not homing
 // enable this option. Override at any time with M120, M121.
-//#define ENDSTOPS_ALWAYS_ON_DEFAULT
+#define ENDSTOPS_ALWAYS_ON_DEFAULT
 
 // @section extras
 
@@ -536,7 +536,7 @@
   #endif
 #endif
 
-//#define Y_DUAL_STEPPER_DRIVERS
+#define Y_DUAL_STEPPER_DRIVERS
 #if ENABLED(Y_DUAL_STEPPER_DRIVERS)
   #define INVERT_Y2_VS_Y_DIR true   // Set 'true' if Y motors should rotate in opposite directions
   //#define Y_DUAL_ENDSTOPS
@@ -1771,7 +1771,7 @@
  * and optionally G38.4 and G38.5 (probe away from target).
  * Set MULTIPLE_PROBING for G38 to probe more than once.
  */
-//#define G38_PROBE_TARGET
+#define G38_PROBE_TARGET
 #if ENABLED(G38_PROBE_TARGET)
   //#define G38_PROBE_AWAY        // Include G38.4 and G38.5 to probe away from target
   #define G38_MINIMUM_MOVE 0.0275 // (mm) Minimum distance that will produce a move.
