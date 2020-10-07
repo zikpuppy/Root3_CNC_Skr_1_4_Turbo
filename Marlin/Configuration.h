aef4d590f7c6175cb912bd4b6b8a5d7fd38a79ce
Mark Jarvis <jarkmarvis@gmail.com>
2020-10-06 23:43:40 -0400

    Adding Z PROBE pin setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -869,7 +869,7 @@
  *      - normally-open switches to 5V and D32.
  *
  */
-//#define Z_MIN_PROBE_PIN 32 // Pin 32 is the RAMPS default
+#define Z_MIN_PROBE_PIN P0_10 //32 // Pin 32 is the RAMPS default
 
 /**
  * Probe Type
